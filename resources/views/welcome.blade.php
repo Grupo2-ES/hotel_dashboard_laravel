b2dc4e90c4b9f69b929851bfdb16c1d0638b385b
rodmoreira41 <a039291@ismai.pt>
2022-06-18 17:33:24 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,16 +23,16 @@
         <div class="w3-bar-block">
         @if (Route::has('login'))
             <div class="hidden fixed top-0 left-0 px-6 py-4 sm:block">
-                    <a class="text-sm text-gray-700 dark:text-gray-500 underline" href="{{ url('employee') }}">Funcionários</a>
+                    <a class="text-sm text-gray-700 dark:text-gray-500 underline" href="{{ route('login') }}">Funcionários</a>
             </div>
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ route('register') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <a href=" " class="text-sm text-gray-700 dark:text-gray-500 underline">Log In Clientes</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ url('/home') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
                 </div>
