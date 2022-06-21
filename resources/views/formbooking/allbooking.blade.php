e7832c1c64121fc239e14aa9a3dc046e7e7ca19b
AnaF <82066444+AnaAzevedo2@users.noreply.github.com>
2022-06-21 19:13:26 +0100

    Update allbooking.blade.php

--- resources/views/formbooking/allbooking.blade.php
+++ resources/views/formbooking/allbooking.blade.php
@@ -12,7 +12,7 @@
                     <div class="col">
                         <div class="mt-5">
                             <h4 class="card-title float-left mt-2">Reservas</h4>
-                            <a href="{{ route('form/booking/add') }}" class="btn btn-primary float-right veiwbutton ">Adicionar uma Reserva</a>
+                            <a href="{{ route('form/booking/add') }}" class="btn btn-primary float-right veiwbutton ">Adicionar Reserva</a>
                         </div>
                     </div>
                 </div>
@@ -25,44 +25,36 @@
                                 <table class="datatable table table-stripped table table-hover table-center mb-0">
                                     <thead>
                                         <tr>
-                                            <th>ID da Reserva</th>
-                                            <th>Nome do Cliente</th>
-                                            <th>Tipo de Quarto</th>
+                                            <th>Reserva ID</th>
+                                            <th>Nome</th>
+                                            <th>Tipo de quarto</th>
                                             <th>Número de Pessoas</th>
-                                            <th>Data</th>
-                                            <th>Hora</th>
                                             <th>Data de Chegada</th>
                                             <th>Data de Partida</th>
                                             <th>Email</th>
-                                            <th>Contacto Telefónico</th>
-                                            <th>Status</th>
-                                            <th class="text-right">Definições</th>
+                                            <th>Nº de Telefone</th>
+                                            <th>Estado</th>
+                                            <th class="text-right">Ações</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($allBookings as $bookings )
                                         <tr>
                                             <td hidden class="id">{{ $bookings->id }}</td>
-                                            <td hidden class="fileupload">{{ $bookings->fileupload }}</td>
                                             <td>{{ $bookings->bkg_id }}</td>
                                             <td>
                                                 <h2 class="table-avatar">
-                                                <a href="profile.html" class="avatar avatar-sm mr-2">
-                                                    <img class="avatar-img rounded-circle" src="{{ URL::to('/assets/upload/'.$bookings->fileupload) }}" alt="{{ $bookings->fileupload }}">
-                                                </a>
-                                                <a href="profile.html">{{ $bookings->name }}<span>{{ $bookings->bkg_id }}</span></a>
+                                                <a href="profile.html">{{ $bookings->name }}</a>
                                                 </h2>
                                             </td>
                                             <td>{{ $bookings->room_type }}</td>
                                             <td>{{ $bookings->total_numbers }}</td>
-                                            <td>{{ $bookings->date }}</td>
-                                            <td>{{ $bookings->time }}</td>
                                             <td>{{ $bookings->arrival_date }}</td>
                                             <td>{{ $bookings->depature_date }}</td>
                                             <td><a href="#" class="__cf_email__" data-cfemail="2652494b4b5f44435448474a66435e474b564a430845494b">{{ $bookings->email }}</a></td>
                                             <td>{{ $bookings->ph_number }}</td>
                                             <td>
-                                                <div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Active</a> </div>
+                                                <div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Ativo</a> </div>
                                             </td>
                                             <td class="text-right">
                                                 <div class="dropdown dropdown-action">
@@ -75,7 +67,7 @@
                                                         </a>
                                                         <a class="dropdown-item bookingDelete" href="#" data-toggle="modal" data-target="#delete_asset">
                                                             <i class="fas fa-trash-alt m-r-5"></i> Apagar
-                                                        </a> 
+                                                        </a>
                                                     </div>
                                                 </div>
                                             </td>
@@ -96,7 +88,7 @@
                     <form action="{{ route('form/booking/delete') }}" method="POST">
                         @csrf
                         <div class="modal-body text-center"> <img src="{{ URL::to('assets/img/sent.png') }}" alt="" width="50" height="46">
-                            <h3 class="delete_class">Tem a certeza que quer eliminar este registo de reserva?</h3>
+                            <h3 class="delete_class">Tem a certeza que quer apagar a reserva ?</h3>
                             <div class="m-t-20">
                                 <a href="#" class="btn btn-white" data-dismiss="modal">Cancelar</a>
                                 <input class="form-control" type="hidden" id="e_id" name="id" value="">
@@ -117,7 +109,6 @@
         {
             var _this = $(this).parents('tr');
             $('#e_id').val(_this.find('.id').text());
-            $('#e_fileupload').val(_this.find('.fileupload').text());
         });
     </script>
     @endsection
