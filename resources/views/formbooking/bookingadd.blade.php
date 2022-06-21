475c88325bf0c21739ab5c5a8036c66e2b684a65
AnaF <82066444+AnaAzevedo2@users.noreply.github.com>
2022-06-21 19:13:48 +0100

    Update bookingadd.blade.php

--- resources/views/formbooking/bookingadd.blade.php
+++ resources/views/formbooking/bookingadd.blade.php
@@ -21,9 +21,9 @@
                         <div class="row formtype">
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label>Name</label>
+                                    <label>Nome</label>
                                     <select class="form-control @error('name') is-invalid @enderror" id="sel1" name="name" value="{{ old('name') }}">
-                                        <option selected disabled> --Select Name-- </option>
+                                        <option selected disabled> --Selecionar Nome-- </option>
                                         @foreach ($user as $users )
                                         <option value="{{ $users->name }}">{{ $users->name }}</option>
                                         @endforeach
@@ -32,9 +32,9 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label>Room Type</label>
+                                    <label>Tipo de Quarto</label>
                                     <select class="form-control @error('room_type') is-invalid @enderror" id="sel2" name="room_type">
-                                        <option selected disabled> --Select Room Type-- </option>
+                                        <option selected disabled> --Selecionar o Tipo de Quarto-- </option>
                                         @foreach ($data as $items )
                                         <option value="{{ $items->room_name }}">{{ $items->room_name }}</option>
                                         @endforeach
@@ -43,29 +43,13 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label>Total Members</label>
+                                    <label>Número de Pessoas</label>
                                     <input type="number" class="form-control @error('total_numbers') is-invalid @enderror"name="total_numbers" value="{{ old('total_numbers') }}">
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label>Date</label>
-                                    <div class="cal-icon">
-                                        <input type="text" class="form-control datetimepicker @error('date') is-invalid @enderror"name="date" value="{{ old('date') }}">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label>Time</label>
-                                    <div class="time-icon">
-                                        <input type="text" class="form-control @error('time') is-invalid @enderror" id="datetimepicker3" name="time" value="{{ old('time') }}">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label>Arrival Date</label>
+                                    <label>Data de Chegada</label>
                                     <div class="cal-icon">
                                         <input type="text" class="form-control datetimepicker @error('arrival_date') is-invalid @enderror" name="arrival_date" value="{{ old('arrival_date') }}">
                                     </div>
@@ -73,9 +57,9 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label>Depature Date</label>
+                                    <label>Data de Partida</label>
                                     <div class="cal-icon">
-                                        <input type="text" class="form-control datetimepicker @error('depature_date') is-invalid @enderror" name="depature_date" value="{{ old('depature_date') }}"> 
+                                        <input type="text" class="form-control datetimepicker @error('depature_date') is-invalid @enderror" name="depature_date" value="{{ old('depature_date') }}">
                                     </div>
                                 </div>
                             </div>
@@ -87,29 +71,20 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label>Phone Number</label>
+                                    <label>Nº de Telefone</label>
                                     <input type="text" class="form-control @error('phone_number') is-invalid @enderror" id="usr1" name="phone_number" value="{{ old('phone_number') }}">
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label>File Upload</label>
-                                    <div class="custom-file mb-3">
-                                        <input type="file" class="custom-file-input @error('fileupload') is-invalid @enderror" id="customFile" name="fileupload" value="{{ old('fileupload') }}">
-                                        <label class="custom-file-label" for="customFile">Choose file</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label>Message</label>
+                                    <label>Mensagem</label>
                                     <textarea class="form-control @error('message') is-invalid @enderror" rows="1.5" id="message" name="message" value="{{ old('message') }}"></textarea>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary buttonedit1">Create Booking</button>
+                <button type="submit" class="btn btn-primary buttonedit1">Criar Reserva</button>
             </form>
         </div>
     </div>
@@ -122,5 +97,5 @@
         });
         </script>
     @endsection
-    
+
 @endsection
